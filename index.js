4781152861c8c7eba67d0885ec38af437971e854
OlhaLyvyndovych <olha.lyvyndovych@gmail.com>
2022-09-15 12:37:14 +0200

    Add .env file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { ethers } = require('ethers');
 
 async function connect() {
@@ -12,95 +13,7 @@ async function execute() {
     // function ✅
     // node connection - for this case it's a metamask ✅
     const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
-    const abi = [
-        {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "_name",
-              "type": "string"
-            },
-            {
-              "internalType": "uint256",
-              "name": "_favoriteNumber",
-              "type": "uint256"
-            }
-          ],
-          "name": "addPerson",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "name": "nameToFavoriteNumber",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "name": "people",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "favoriteNumber",
-              "type": "uint256"
-            },
-            {
-              "internalType": "string",
-              "name": "name",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "retrieve",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "_favoriteNumber",
-              "type": "uint256"
-            }
-          ],
-          "name": "store",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        }
-      ]
+    const abi = process.env.ABI
       // Here is the connection of metamask / ethers / 
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       // To get the connected account
